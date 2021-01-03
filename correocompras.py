d118e98c86bbbf69edb3aa3406338c4549fe1444
Guille <guillearg@gmail.com>
2021-01-03 18:17:45 -0300

    loop products

--- correocompras.py
+++ correocompras.py
@@ -6,7 +6,7 @@ from data import *
 import time
 import requests
 from bs4 import BeautifulSoup
-
+import json
 import pandas as pd
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -14,6 +14,7 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get(base_url)
 driver.refresh()
 WebDriverWait(driver, 20)
+
 menu = driver.find_elements_by_class_name('cat-n2')
 
 categories = {}
@@ -24,8 +25,6 @@ for category in menu:
     subcategories = {}
     for subcategory in category.find_elements_by_class_name('cat-n2-list-item'):
         try:
-            print(subcategory.find_element_by_tag_name(
-                'a').get_attribute('text'))
             subcategories[subcategory.find_element_by_tag_name('a').get_attribute(
                 'text')] = subcategory.find_element_by_tag_name('a').get_attribute('href')
         except:
@@ -33,5 +32,47 @@ for category in menu:
 
     categories[category_name] = subcategories
 
+categorias = {}
+for category in list(categories.keys()):
+    productos = {}
+    for subcategory in categories[category]:
+        driver.get(categories[category][subcategory])
+        speed = 15
+        current_scroll_position, new_height = 0, 1
+        while current_scroll_position <= new_height:
+            current_scroll_position += speed
+            driver.execute_script(
+                "window.scrollTo(0, {});".format(current_scroll_position))
+            new_height = driver.execute_script(
+                "return document.body.scrollHeight")
+
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+
+        products = soup.find('div', {'class': 'product-list'})
+
+        subprod = {}
+
+        try:
+            for product in products.findAll('ul'):
+                try:
+                    id = product.find(
+                        'div', {'class': 'box-item'})['id'].split('-')[1]
+                    name = product.find(
+                        'span', {'class': 'product-description'}).text
+                    price = product.find(
+                        'span', {'class': "best-price"}).text.split('$')[1]
+                    url = product.find('a', {'class': "product-link"})['href']
+                    subprod[str(id)] = {'name': name,
+                                        'price': price, 'url': url}
+                except:
+                    pass
+        except:
+            print(subcategory,  ' Error No data')
+
+        productos[subcategory] = subprod
+    categorias[category] = productos
+
+with open('products.json', 'w') as fp:
+    json.dump(categorias, fp)
+
 driver.close()
-#print(categories)
